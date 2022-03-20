e230af2ea996aecc5ef9ba22e64c5036a2a67551
valeria <valeria.granada@evertecinc.com>
2022-03-20 02:07:55 -0500

    styles: implement styles

---
+++ src/presentation/assets/utils/navigation.js
@@ -0,0 +1,26 @@
+export const navigation = [
+    {
+        name: 'Hombre',
+        url: ''
+    },
+    {
+        name: 'Mujer',
+        url: ''
+    },
+    {
+        name: 'Junior',
+        url: ''
+    },
+    {
+        name: 'Niños',
+        url: ''
+    },
+    {
+        name: 'Accesorios',
+        url: ''
+    },
+    {
+        name: 'Ofertas',
+        url: ''
+    },
+];
